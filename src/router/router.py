6a5e92e9eba49a17286ccea794aaf892342fc4d0
zlicdt <zhuhaoming135@outlook.com>
2025-11-20 13:33:23 +0800

    fix: Avoid CORS block

--- src/router/router.py
+++ src/router/router.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from .dependencies import auth_router
 from .routes import api_router
+
+import os
 
 router = FastAPI()
 router.include_router(auth_router)
 router.include_router(api_router)
+router.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:" + os.getenv("WEB_PORT", "8192")],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
